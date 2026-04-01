b3e37ba930ce302a098af550f32b9d88b4b76411
ella <lydbsdud@gmail.com>
2026-04-01 21:01:28 +0900

    feat: initial project setup

---
+++ src/main/kotlin/com/team2/team2backend/Team2BackendApplication.kt
@@ -0,0 +1,11 @@
+package com.team2.team2backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Team2BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<Team2BackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/team2/team2backend/Team2BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.team2.team2backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Team2BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
