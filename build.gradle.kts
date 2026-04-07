b8eb27a11e11788ac11237cfe21413cd709d3601
ella <lydbsdud@gmail.com>
2026-04-08 01:11:35 +0900

    chore: detekt 버전 업그레이드 (Kotlin 2.3.x 호환)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "2.2.21"
     id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
-    id("io.gitlab.arturbosch.detekt") version "1.23.7"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
     jacoco
 }
 
