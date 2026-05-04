95fa011c57d91e5b87783955e600c5509f6d39cd
eundeang <111877048+eundeang@users.noreply.github.com>
2026-05-04 19:44:24 +0900

    fix: 파티 삭제용 Repository 메서드에 @Modifying 추가

--- src/main/kotlin/com/team2/server/party/repository/PartyInviteRepository.kt
+++ src/main/kotlin/com/team2/server/party/repository/PartyInviteRepository.kt
@@ -2,6 +2,8 @@ package com.team2.server.party.repository
 
 import com.team2.server.party.entity.PartyInvite
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 
 interface PartyInviteRepository : JpaRepository<PartyInvite, Long> {
@@ -12,5 +14,7 @@ interface PartyInviteRepository : JpaRepository<PartyInvite, Long> {
         now: LocalDateTime,
     ): PartyInvite?
 
+    @Modifying
+    @Transactional
     fun deleteAllByPartyId(partyId: Long)
 }
--- src/main/kotlin/com/team2/server/party/repository/RealtimeParticipantProfileRepository.kt
+++ src/main/kotlin/com/team2/server/party/repository/RealtimeParticipantProfileRepository.kt
@@ -3,9 +3,13 @@ package com.team2.server.party.repository
 import com.team2.server.party.entity.Participant
 import com.team2.server.party.entity.RealtimeParticipantProfile
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.transaction.annotation.Transactional
 
 interface RealtimeParticipantProfileRepository : JpaRepository<RealtimeParticipantProfile, Long> {
     fun findByParticipant(participant: Participant): RealtimeParticipantProfile?
 
+    @Modifying
+    @Transactional
     fun deleteAllByParticipantIdIn(participantIds: List<Long>)
 }
