b4dca51ccebf24c0682642b19f0aac7aec5dba98
Tae gyu Choi <suker800@gmail.com>
2026-05-14 23:02:12 +0900

    fix: ApiResponse import 경로 변경 반영

--- src/main/kotlin/com/team2/server/me/api/MeAccountController.kt
+++ src/main/kotlin/com/team2/server/me/api/MeAccountController.kt
@@ -1,7 +1,7 @@
 package com.team2.server.me.api
 
 import com.team2.server.auth.principal.UserPrincipal
-import com.team2.server.common.response.ApiResponse
+import com.team2.server.common.web.ApiResponse
 import com.team2.server.me.api.dto.MeAccountResponse
 import com.team2.server.me.application.usecase.GetMeAccountUseCase
 import org.springframework.security.core.annotation.AuthenticationPrincipal
