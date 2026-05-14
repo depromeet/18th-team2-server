44feb3c7db69fd847e04496822a68f9017ed950c
Tae gyu Choi <suker800@gmail.com>
2026-05-14 20:42:06 +0900

    chore: ArchUnit FEATURES 목록에 me 추가

--- src/test/kotlin/com/team2/server/architecture/ArchUnitConstants.kt
+++ src/test/kotlin/com/team2/server/architecture/ArchUnitConstants.kt
@@ -3,7 +3,7 @@ package com.team2.server.architecture
 object ArchUnitConstants {
     const val BASE_PACKAGE = "com.team2.server"
 
-    val FEATURES = listOf("auth", "user", "party", "image", "chat", "rollingpaper")
+    val FEATURES = listOf("auth", "user", "party", "image", "chat", "rollingpaper", "me")
 
     const val API = "..api.."
     const val USECASE = "..application.usecase.."
