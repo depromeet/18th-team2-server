690fde044aa3c7141966fef3372edcbbf43a4c17
eundeang <111877048+eundeang@users.noreply.github.com>
2026-06-08 19:50:42 +0800

    fix: use runtime default timezone

--- src/main/kotlin/com/team2/server/common/config/TimeConfig.kt
+++ src/main/kotlin/com/team2/server/common/config/TimeConfig.kt
@@ -1,23 +1,11 @@
 package com.team2.server.common.config
 
-import jakarta.annotation.PostConstruct
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Clock
-import java.time.ZoneId
-import java.util.TimeZone
 
 @Configuration
 class TimeConfig {
-    @PostConstruct
-    fun setDefaultTimeZone() {
-        TimeZone.setDefault(TimeZone.getTimeZone(SEOUL_ZONE_ID))
-    }
-
     @Bean
-    fun clock(): Clock = Clock.system(ZoneId.of(SEOUL_ZONE_ID))
-
-    companion object {
-        const val SEOUL_ZONE_ID = "Asia/Seoul"
-    }
+    fun clock(): Clock = Clock.systemDefaultZone()
 }
